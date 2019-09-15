68dfe178ffdf407f69151bcb32933b2a6a402e31
ZHANG Yi <45352743+BillYiZhang@users.noreply.github.com>
2019-09-15 21:43:49 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #------------------------ load the necessary packages ------------------------
 import os
+import torch
 pj_path = os.getcwd()
 
 #########################################################################
@@ -39,15 +40,27 @@ ids_objms_train_path = pj_path + '/Data/objms_train.txt'
 ids_objms_val_path = pj_path + '/Data/objms_val.txt'
 ids_objms_test_path = pj_path + '/Data/objms_test.txt'
 
+# path of log for model training
+log_dir = pj_path + '/Log'
+
+# path for model saving
+mod_dir = pj_path + '/Log/net_params.pkl'
+
 #########################################################################
 # PARAMETERS      														#
 #########################################################################
 update_dts = 0
 train_on = 1
-test_on =1
+test_on = 0
 size_train = 896
 scale_crop = 1
 num_train = 51
-batch_size = 2
-Epochs = 10
+batch_size = 1
+Epochs = 2
 debug_on = 1
+lr_init = 0.001
+epsilon_DC = 0.001
+step_num = 10
+pretrain_on = True
+num_classes = 1
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
