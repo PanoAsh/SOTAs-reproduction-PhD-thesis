8808e3972533a82957119025657935867a30ed9d
ZHANG Yi <45352743+Panorama-Bill@users.noreply.github.com>
2019-11-28 18:39:57 +0100

    Add files via upload

--- json_to_dataset.py
+++ json_to_dataset.py
@@ -11,17 +11,29 @@ class file_arrangement():
         self.path_yaml = os.getcwd() + '/yamls'
         self.path_instance = os.getcwd() + '/salient_instance'
         self.path_object = os.getcwd() + '/salient_object'
+        self.path_stimuli = os.getcwd() + '/stimuli'
 
     def arrange_labels(self):
         for item1 in listdir(self.path):
             if item1.endswith('_json'):
                 label_path = os.path.join(os.path.abspath(self.path), item1)
 
+                for item0 in listdir(label_path):
+                    if item0.endswith('img.png'):
+                        src = os.path.join(os.path.abspath(label_path), item0)
+                        dst = os.path.join(os.path.abspath(self.path_stimuli),
+                                           item1[0:3] + '.png')
+                        try:
+                            os.rename(src, dst)
+                            print('converting %s to %s ...' % (src, dst))
+                        except:
+                            continue
+
                 for item2 in listdir(label_path):
                     if item2.endswith('label.png'):
                         src = os.path.join(os.path.abspath(label_path), item2)
                         dst = os.path.join(os.path.abspath(self.path_labels),
-                                           item1[0:7] + '.png')
+                                           item1[0:3] + '.png')
                         try:
                             os.rename(src, dst)
                             print('converting %s to %s ...' % (src, dst))
@@ -32,7 +44,7 @@ class file_arrangement():
                     if item3.endswith('viz.png'):
                         src = os.path.join(os.path.abspath(label_path), item3)
                         dst = os.path.join(os.path.abspath(self.path_overlays),
-                                           item1[0:7] + '.png')
+                                           item1[0:3] + '.png')
                         try:
                             os.rename(src, dst)
                             print('converting %s to %s ...' % (src, dst))
@@ -43,7 +55,7 @@ class file_arrangement():
                     if item4.endswith('.txt'):
                         src = os.path.join(os.path.abspath(label_path), item4)
                         dst = os.path.join(os.path.abspath(self.path_txt),
-                                           item1[0:7] + '.txt')
+                                           item1[0:3] + '.txt')
                         try:
                             os.rename(src, dst)
                             print('converting %s to %s ...' % (src, dst))
@@ -54,7 +66,7 @@ class file_arrangement():
                     if item5.endswith('.yaml'):
                         src = os.path.join(os.path.abspath(label_path), item5)
                         dst = os.path.join(os.path.abspath(self.path_yaml),
-                                           item1[0:7] + '.yaml')
+                                           item1[0:3] + '.yaml')
                         try:
                             os.rename(src, dst)
                             print('converting %s to %s ...' % (src, dst))
@@ -81,7 +93,25 @@ class file_arrangement():
             print(" {} imgs processed".format(count))
             count += 1
 
+    def rename(self):
+        filelist = os.listdir(self.path)
+        filelist.sort(key=lambda x: x[:-5])
+
+        count = 1
+        for item in filelist:
+            if item.endswith('.json'):
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(os.path.abspath(self.path), format(str(count), '0>3s')+'.json')
+                try:
+                    os.rename(src, dst)
+                    print ('converting %s to %s ...' % (src, dst))
+                except:
+                    continue
+                print(" {} images processed".format(count))
+                count += 1
+
 if __name__ == '__main__':
     file_arranger = file_arrangement()
-    # file_arranger.arrange_labels()
+   # file_arranger.rename()
+   # file_arranger.arrange_labels()
     file_arranger.instance_to_object()
