70195289e558136dc1ec8d4cef078465faddcf39
ZHANG Yi <45352743+Panorama-Bill@users.noreply.github.com>
2019-12-13 16:06:42 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -19,6 +19,23 @@ R_PATH_RAW = PROJECT_ROOT + '/fixpos/scanpath_nante/R/'
 L_PATH_TGT = PROJECT_ROOT + '/fixpos/fixations/L/'
 R_PATH_TGT = PROJECT_ROOT + '/fixpos/fixations/R/'
 
+CMP_PATH = PROJECT_ROOT + '/cubmaps/'
+ERP_PATH = PROJECT_ROOT + '/stimulis/'
+
+TRAIN_TXT_PATH = PROJECT_ROOT + '/train.txt'
+TEST_TXT_PATH = PROJECT_ROOT + '/test.txt'
+
+EASY_TXT_PATH = PROJECT_ROOT + '/easy.txt'
+MEDIUM_TXT_PATH = PROJECT_ROOT + '/medium.txt'
+HARD_TXT_PATH = PROJECT_ROOT + '/hard.txt'
+
+PANOISOD_IMG_PATH = PROJECT_ROOT + '/360ISOD_img/'
+PANOISOD_MSK_PATH = PROJECT_ROOT + '/360ISOD_msk/'
+PANOISOD_IMG_TRAIN_PATH = PROJECT_ROOT + '/360ISOD_img_train/'
+PANOISOD_MSK_TRAIN_PATH = PROJECT_ROOT + '/360ISOD_msk_train/'
+PANOISOD_IMG_TEST_PATH = PROJECT_ROOT + '/360ISOD_img_test/'
+PANOISOD_MSK_TEST_PATH = PROJECT_ROOT + '/360ISOD_msk_test/'
+
 # Default of the 360ISOD dataset
 width_360ISOD = 2048
 height_360ISOD = 1024
