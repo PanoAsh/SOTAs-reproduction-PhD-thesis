c8b5e48a833df97a160a571ba316c4a326793e8d
ZHANG Yi <45352743+PanoAsh@users.noreply.github.com>
2020-07-06 21:47:50 +0200

    Add files via upload

---
+++ 360VSOD/omnivsod/run.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+from dataset import get_loader
+from solver import Solver
+
+
+def main(config):
+    if config.mode == 'train':
+        train_loader, dataset = get_loader(config.batch_size, num_thread=config.num_thread)
+        run = "omnivsod"
+        if not os.path.exists("%s/run-%s" % (config.save_fold, run)): 
+            os.mkdir("%s/run-%s" % (config.save_fold, run))
+            os.mkdir("%s/run-%s/logs" % (config.save_fold, run))
+            os.mkdir("%s/run-%s/models" % (config.save_fold, run))
+        config.save_fold = "%s/run-%s" % (config.save_fold, run)
+        train = Solver(train_loader, None, config)
+        train.train()
+    elif config.mode == 'test':
+        test_loader, dataset = get_loader(config.test_batch_size, mode='test',num_thread=config.num_thread)
+        test = Solver(None, test_loader, config, dataset.save_folder())
+        test.test()
+    else:
+        raise IOError("illegal input!!!")
+
+
+if __name__ == '__main__':
+
+    resnet_path = os.getcwd() + '/pretrained/resnet50-19c8e357.pth'
+
+    test_model_path = os.getcwd() + '/results/models/..'
+    test_save_path = os.getcwd() + '/results/predicted/'
+
+    parser = argparse.ArgumentParser()
+
+    # Hyper-parameters
+    parser.add_argument('--n_color', type=int, default=3)
+
+    parser.add_argument('--cuda', type=bool, default=True)
+
+    # Training settings
+    parser.add_argument('--resnet', type=str, default=resnet_path)
+    parser.add_argument('--epoch', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--test_batch_size', type=int, default=1)
+    parser.add_argument('--num_thread', type=int, default=1)
+    parser.add_argument('--save_fold', type=str, default='./results')
+    parser.add_argument('--epoch_save', type=int, default=1)
+    parser.add_argument('--epoch_show', type=int, default=1)
+    parser.add_argument('--pre_trained', type=str, default='')
+
+    # Testing settings
+    parser.add_argument('--model', type=str, default=test_model_path)
+    parser.add_argument('--test_fold', type=str, default=test_save_path)
+
+    # Misc
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    parser.add_argument('--visdom', type=bool, default=False)
+    
+    config = parser.parse_args()
+
+    main(config)
