f4135084269ae23359e68695dfb07fa9376c699c
ZHANG Yi <45352743+PanoAsh@users.noreply.github.com>
2020-08-19 18:40:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,104 +1,44 @@
+import torch
+import torch.nn as nn
 import argparse
+import os.path as osp
 import os
-from dataset.dataset import get_loader
-from solver import Solver
-
-def get_test_info(sal_mode='vr'):
-    if sal_mode == 'e':
-        image_root = './data/ECSSD/Imgs/'
-        image_source = './data/ECSSD/test.lst'
-    elif sal_mode == 'p':
-        image_root = './data/PASCALS/Imgs/'
-        image_source = './data/PASCALS/test.lst'
-    elif sal_mode == 'd':
-        image_root = './data/DUTOMRON/Imgs/'
-        image_source = './data/DUTOMRON/test.lst'
-    elif sal_mode == 'h':
-        image_root = './data/HKU-IS/Imgs/'
-        image_source = './data/HKU-IS/test.lst'
-    elif sal_mode == 's':
-        image_root = './data/SOD/Imgs/'
-        image_source = './data/SOD/test.lst'
-    elif sal_mode == 't':
-        image_root = './data/DUTS-TE/Imgs/'
-        image_source = './data/DUTS-TE/test.lst'
-    elif sal_mode == 'm_r': # for speed test
-        image_root = './data/MSRA/Imgs_resized/'
-        image_source = './data/MSRA/test_resized.lst'
-
-    elif sal_mode == 'vr': # 360ISOD dataset for testing
-        image_root = './data/360ISOD-TE/Imgs/'
-        image_source = './data/360ISOD-TE/test.lst'
-
-    return image_root, image_source
-
-def main(config):
-    if config.mode == 'train':
-        train_loader = get_loader(config)
-        run = 0
-        while os.path.exists("%s/run-%d" % (config.save_folder, run)):
-            run += 1
-        os.mkdir("%s/run-%d" % (config.save_folder, run))
-        os.mkdir("%s/run-%d/models" % (config.save_folder, run))
-        config.save_folder = "%s/run-%d" % (config.save_folder, run)
-        train = Solver(train_loader, None, config)
-        train.train()
-    elif config.mode == 'test':
-        config.test_root, config.test_list = get_test_info(config.sal_mode)
-        test_loader = get_loader(config, mode='test')
-        if not os.path.exists(config.test_fold): os.mkdir(config.test_fold)
-        test = Solver(None, test_loader, config)
-        test.test()
+from evaluator import Eval_thread
+from dataloader import EvalDataset
+# from concurrent.futures import ThreadPoolExecutor
+def main(cfg):
+    root_dir = cfg.root_dir
+    if cfg.save_dir is not None:
+        output_dir = cfg.save_dir
     else:
-        raise IOError("illegal input!!!")
+        output_dir = root_dir
+    gt_dir = osp.join(root_dir, 'gt')
+    pred_dir = osp.join(root_dir, 'pred')
+    if cfg.methods is None:
+        method_names = os.listdir(pred_dir)
+    else:
+        method_names = cfg.methods.split(' ')
+    if cfg.datasets is None:
+        dataset_names = os.listdir(gt_dir)
+    else:
+        dataset_names = cfg.datasets.split(' ')
+    
+    threads = []
+    for dataset in dataset_names:
+        for method in method_names:
+            loader = EvalDataset(osp.join(pred_dir, method, dataset), osp.join(gt_dir, dataset))
+            thread = Eval_thread(loader, method, dataset, output_dir, cfg.cuda)
+            threads.append(thread)
+    for thread in threads:
+        print(thread.run())
 
-if __name__ == '__main__':
-
-    vgg_path = './dataset/pretrained/vgg16_20M.pth'
-    resnet_path = './dataset/pretrained/resnet50_caffe.pth'
-    resnet_wo_path = './dataset/pretrained/final_run0.pth'
-    resnet_w_path = './dataset/pretrained/final_run1.pth'
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    # Hyper-parameters
-    parser.add_argument('--n_color', type=int, default=3)
-    # 1/10 of the original lr, to avoid the overfitting due to the overlap of multi-cubes
-    parser.add_argument('--lr', type=float, default=5e-6) # Learning rate resnet:5e-5, vgg:1e-4
-    parser.add_argument('--wd', type=float, default=0.0005) # Weight decay
-    parser.add_argument('--no-cuda', dest='cuda', action='store_false')
-
-    # Training settings
-    parser.add_argument('--arch', type=str, default='resnet') # resnet or vgg
-    parser.add_argument('--pretrained_model', type=str, default=resnet_path)
-    parser.add_argument('--epoch', type=int, default=70)
-    parser.add_argument('--batch_size', type=int, default=1) # only support 1 now
-    parser.add_argument('--num_thread', type=int, default=1)
-    parser.add_argument('--load', type=str, default=resnet_wo_path)
-    parser.add_argument('--save_folder', type=str, default='./results')
-    parser.add_argument('--epoch_save', type=int, default=3)
-    parser.add_argument('--iter_size', type=int, default=10)
-    parser.add_argument('--show_every', type=int, default=50)
-
-    # Train data
-    parser.add_argument('--train_root', type=str, default='./data/360ISOD')
-    parser.add_argument('--train_list', type=str, default='./data/360ISOD/train_pair.lst')
-
-    # Testing settings
-    parser.add_argument('--model', type=str, default='./results/run-360/models/final.pth') # Snapshot
-    parser.add_argument('--test_fold', type=str, default='./results/run-360/run-360-sal') # Test results saving folder
-    parser.add_argument('--sal_mode', type=str, default='vr') # Test image dataset
-
-    # Misc
-    parser.add_argument('--mode', type=str, default='test', choices=['train', 'test'])
+    parser.add_argument('--methods', type=str, default='AADFNet BASNet COSNet CPD CSFRes2Net CSNet EGNet F3Net '
+                                                       'GCPANet MGA MINet PoolNet RAS RCRNet ScribbleSOD SCRN')
+    parser.add_argument('--datasets', type=str, default='HSAV360')
+    parser.add_argument('--root_dir', type=str, default=os.getcwd())
+    parser.add_argument('--save_dir', type=str, default=os.getcwd() + '/score/')
+    parser.add_argument('--cuda', type=bool, default=True)
     config = parser.parse_args()
-
-    if not os.path.exists(config.save_folder):
-        os.mkdir(config.save_folder)
-
-    # Get test set info
-    test_root, test_list = get_test_info(config.sal_mode)
-    config.test_root = test_root
-    config.test_list = test_list
-
     main(config)
