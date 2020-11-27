6f59eb622ca6048e545cb2392908918b60117ec9
ZHANG Yi <45352743+PanoAsh@users.noreply.github.com>
2020-11-27 04:30:49 +0100

    Add files via upload

--- LFSOD-main/defocus_maps.py
+++ LFSOD-main/defocus_maps.py
@@ -18,13 +18,14 @@ image_root = os.getcwd() + '/data/FS_rgb_train/'
 gt_root = os.getcwd() + '/data/GT_train/'
 test_loader = test_dataset(image_root, gt_root)
 
-save_path_depth = os.getcwd() + '/data/depth_train/'
+#save_path_depth = os.getcwd() + '/data/depth_test/'
 save_path_defocus = os.getcwd() + '/data/defocus_train/'
+save_path_FS = os.getcwd() + '/data/FS_order_train/'
 
 model.eval()
 with torch.no_grad():
     for i in range(test_loader.size):
-        images, gt, name = test_loader.load_data()
+        images, gt, name, fs_name = test_loader.load_data()
         defocus_list, depth_list = [], []
         for idx in range(len(images)):
             images[idx] = images[idx].cuda()
@@ -39,19 +40,23 @@ with torch.no_grad():
             res = res.sigmoid().data.cpu().numpy().squeeze()
             res = (res - res.min()) / (res.max() - res.min() + 1e-8)
             mae_list.append(np.sum(np.abs(res - gt)) * 1.0 / (gt.shape[0] * gt.shape[1]))
-        best_idx = mae_list.index(min(mae_list))
+        #best_idx = mae_list.index(min(mae_list))
+        mae_index = sorted(range(len(mae_list)), key=lambda k: mae_list[k])  # small to large
 
-        res = defocus_list[best_idx]
-        res = res.sigmoid().data.cpu().numpy().squeeze()
-        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        print('save img to: ', save_path_defocus + name)
-        cv2.imwrite(save_path_defocus + name, res * 255)
+        for idx in range(len(images)):
+            res = defocus_list[mae_index[idx]]
+            res = res.sigmoid().data.cpu().numpy().squeeze()
+            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+            print('save img to: ', save_path_defocus + fs_name[mae_index[idx]][:-4] + '.png')
+            cv2.imwrite(save_path_defocus + fs_name[mae_index[idx]][:-4] + '.png', res * 255)
 
-        res = depth_list[best_idx]
-        res = res.sigmoid().data.cpu().numpy().squeeze()
-        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        print('save img to: ', save_path_depth + name)
-        cv2.imwrite(save_path_depth + name, res * 255)
+            fs_pth = os.path.join(image_root, fs_name[mae_index[idx]])
+            new_name = name[:-4] + '_' + format(str(idx), '0>2s') + '.jpg'
+            new_pth = os.path.join(save_path_FS, new_name)
+            print('save img to: ', new_pth)
+            os.rename(fs_pth, new_pth)
+            print()
+
     print('Test Done!')
 
 
